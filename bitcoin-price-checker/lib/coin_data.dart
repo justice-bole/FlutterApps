1001e7f5f88fe2430c4fb7376858a644da572de0
NetworkerLurker <78961523+justice-bole@users.noreply.github.com>
2023-01-17 05:55:54 +0100

    feat: Add BTC, ETH, and LTC exchange rates for various currencies

--- bitcoin-price-checker/lib/coin_data.dart
+++ bitcoin-price-checker/lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'package:http/http.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,20 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = '3798E3A4-F90D-4AEA-A38B-3570DE107A4F';
+const url = 'http://rest.coinapi.io/v1/exchangerate/';
+//BTC/USD?apikey=3798E3A4-F90D-4AEA-A38B-3570DE107A4F';
+
+class CoinData {
+  Future<double> getCoinData(int index, int coin) async {
+    Response response = await get(Uri.parse(
+        '$url${cryptoList[coin]}/${currenciesList[index]}?apikey=$apiKey'));
+    if (response.statusCode == 200) {
+      var decodedResponse = jsonDecode(response.body);
+      double rate = decodedResponse['rate'];
+      return rate;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
--- bitcoin-price-checker/lib/price_screen.dart
+++ bitcoin-price-checker/lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,84 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String currency = 'USD';
+  CoinData coinData = CoinData();
+  double btc_value = 0.0;
+  double eth_value = 0.0;
+  double ltc_value = 0.0;
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    setValue();
+    super.initState();
+  }
+
+  void setValue() async {
+    btc_value = await coinData.getCoinData(0, 0);
+    eth_value = await coinData.getCoinData(0, 1);
+    ltc_value = await coinData.getCoinData(0, 2);
+  }
+
+  DropdownButton getAndroidPicker() {
+    return DropdownButton(
+      value: currency,
+      items: getDropdownItems(),
+      onChanged: (index) async {
+        currentIndex = index;
+        btc_value = await coinData.getCoinData(index, 0);
+        eth_value = await coinData.getCoinData(index, 1);
+        ltc_value = await coinData.getCoinData(index, 2);
+        setState(() {});
+      },
+    );
+  }
+
+  CupertinoPicker getIOSPicker() {
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) async {
+        currentIndex = selectedIndex;
+        btc_value = await coinData.getCoinData(selectedIndex, 0);
+        eth_value = await coinData.getCoinData(selectedIndex, 1);
+        ltc_value = await coinData.getCoinData(selectedIndex, 2);
+        setState(() {});
+      },
+      backgroundColor: Colors.lightBlue,
+      children: getPickerItems(),
+    );
+  }
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List<DropdownMenuItem> dropdownItems = [];
+
+    for (String currency in currenciesList) {
+      DropdownMenuItem dropdown =
+          DropdownMenuItem(child: Text(currency), value: currency);
+      dropdownItems.add(dropdown);
+    }
+
+    return dropdownItems;
+  }
+
+  List<Widget> getPickerItems() {
+    List<Widget> dropdownItems = [];
+
+    for (String currency in currenciesList) {
+      dropdownItems.add(Text(currency));
+    }
+
+    return dropdownItems;
+  }
+
+  Widget getPlatformPicker() {
+    if (Platform.isIOS) {
+      return getIOSPicker();
+    } else if (Platform.isAndroid) {
+      return getAndroidPicker();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +109,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ${btc_value.toString()} ${currenciesList[currentIndex]}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = ${eth_value.toString()} ${currenciesList[currentIndex]}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = ${ltc_value.toString()} ${currenciesList[currentIndex]}',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +166,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: getPlatformPicker(),
           ),
         ],
       ),
